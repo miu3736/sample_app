92d1f7a411f295cd59e19f5059d9996b11c46a61
Miu <jx20291@ns.kogakuin.ac.jp>
2022-12-04 18:31:59 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#home'
-   # root 'application#hello'
-  get  'static_pages/home'
-  # => StaticPages#home
-  
-  get  'static_pages/help'
-   # => StaticPages#help
-   
-  get  'static_pages/about'
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- 
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
 end
